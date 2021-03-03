ee1680d5100c006c2213f4296e639d4bf0eff00f
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 16:44:48 +0900

    docker test

--- init.sh
+++ init.sh
@@ -2,3 +2,27 @@
 echo "hoge"
 echo `pwd`
 ls -l
+
+ls -l /dev/
+
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+# /etc/apt/sources.listは初回起動時にリセットを食らうので、.dに入れる
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo sh -c "cat << EOF >> /etc/apt/sources.list.d/docker.list
+deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
+EOF"
+
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl restart docker.service 
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker run --rm hello-world
